44b1e52cb053434fce5e44af7ce03ad64bc0dd4d
Eric Jennings <ericj@loopshot.com>
2011-05-31 12:48:48 -0700

    - Added Facebook connector test to confirm total number of events emitted - Removed status/facebook event as details of service-type mapping isn't fleshed out yet, and this is a dependency.

--- Connectors/Facebook/sync-api.js
+++ Connectors/Facebook/sync-api.js
@@ -28,9 +28,6 @@ function authComplete(theauth, mongoCollections) {
     app.get('/newsfeed', newsfeed);
     app.get('/wall', wall);
     
-    sync.eventEmitter.on('status/facebook', function(eventObj) {
-        locker.event('status/facebook', eventObj);
-    });
     sync.eventEmitter.on('contact/facebook', function(eventObj) {
         locker.event('contact/facebook', eventObj);
     });
